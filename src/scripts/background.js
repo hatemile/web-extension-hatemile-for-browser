aeebd3c582b4e6e12400be6608da55ba14a8fc82
3837162+carlsonsantana@users.noreply.github.com <carlsonsantana@gmail.com>
2017-10-19 21:36:36 -0300

    :recycle: Resolve line too long alert

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -3,41 +3,55 @@ loadStyle('accessibleformvalidation.css', 'accessibleformvalidation');
 loadStyle('hide_changes.css', 'hidechanges');
 loadScript('hatemile/js/common.js', 'hatemile-common');
 loadScript('hatemile/js/eventlistener.js', 'eventlistenerlist');
-loadScript('hatemile/js/hatemile/util/CommonFunctions.js', 'hatemile-common-functions');
+loadScript('hatemile/js/hatemile/util/CommonFunctions.js',
+        'hatemile-common-functions');
 loadScript('hatemile/js/hatemile/util/Configure.js', 'hatemile-configure');
-loadScript('hatemile/js/hatemile/util/html/vanilla/VanillaHTMLDOMElement.js', 'hatemile-vanillahtmldomelement');
-loadScript('hatemile/js/hatemile/util/html/vanilla/VanillaHTMLDOMParser.js', 'hatemile-vanillahtmldomparser');
-loadScript('hatemile/js/hatemile/util/html/vanilla/VanillaHTMLDOMTextNode.js', 'hatemile-vanillahtmldomtextnode');
-loadScript('hatemile/js/hatemile/implementation/AccessibleEventImplementation.js', 'hatemile-accessibleevent');
-loadScript('hatemile/js/hatemile/implementation/AccessibleFormImplementation.js', 'hatemile-accessibleform');
+loadScript('hatemile/js/hatemile/util/html/vanilla/VanillaHTMLDOMElement.js',
+        'hatemile-vanillahtmldomelement');
+loadScript('hatemile/js/hatemile/util/html/vanilla/VanillaHTMLDOMParser.js',
+        'hatemile-vanillahtmldomparser');
+loadScript('hatemile/js/hatemile/util/html/vanilla/VanillaHTMLDOMTextNode.js',
+        'hatemile-vanillahtmldomtextnode');
+loadScript('hatemile/js/hatemile/implementation/'
+    + 'AccessibleEventImplementation.js', 'hatemile-accessibleevent');
+loadScript('hatemile/js/hatemile/implementation/'
+    + 'AccessibleFormImplementation.js', 'hatemile-accessibleform');
 loadScript('execute.js', 'hatemile-execute');
 
 window.addEventListener('load', function() {
     getConfiguration(function(configuration) {
         var configure = new hatemile.util.Configure(configuration);
-        var htmlParser = new hatemile.util.html.vanilla.VanillaHTMLDOMParser(document);
+        var htmlParser = new hatemile.util.html.vanilla
+                .VanillaHTMLDOMParser(document);
 
-        var accessibleAssociation = new hatemile.implementation.AccessibleAssociationImplementation(htmlParser, configure);
+        var accessibleAssociation = new hatemile.implementation
+                .AccessibleAssociationImplementation(htmlParser, configure);
         accessibleAssociation.associateAllDataCellsWithHeaderCells();
         accessibleAssociation.associateAllLabelsWithFields();
 
-        var accessibleForm = new hatemile.implementation.AccessibleFormImplementation(htmlParser, configure);
+        var accessibleForm = new hatemile.implementation
+                .AccessibleFormImplementation(htmlParser, configure);
         accessibleForm.markAllAutoCompleteFields();
         accessibleForm.markAllRangeFields();
         accessibleForm.markAllRequiredFields();
 
-        var accessibleNavigation = new hatemile.implementation.AccessibleNavigationImplementation(htmlParser, configure, [
+        var skippers = [
             {
                 'selector': '#container-heading',
                 'description': configuration['skipper-tableofcontents'],
                 'shortcut': '0'
             }
-        ]);
+        ];
+        var accessibleNavigation = new hatemile.implementation
+                .AccessibleNavigationImplementation(htmlParser, configure,
+                        skippers);
         accessibleNavigation.provideNavigationByAllHeadings();
         accessibleNavigation.provideNavigationToAllLongDescriptions();
         accessibleNavigation.provideNavigationByAllSkippers();
 
-        var accessibleDisplay = new hatemile.implementation.AccessibleDisplayScreenReaderImplementation(htmlParser, configure, window.navigator.userAgent);
+        var accessibleDisplay = new hatemile.implementation
+                .AccessibleDisplayScreenReaderImplementation(htmlParser,
+                        configure, window.navigator.userAgent);
         accessibleDisplay.displayAllCellHeaders();
         accessibleDisplay.displayAllWAIARIAStates();
         accessibleDisplay.displayAllLinksAttributes();
--- src/scripts/preferences.js
+++ src/scripts/preferences.js
@@ -3,7 +3,8 @@
     var elements = document.querySelectorAll('[data-i18n]');
     for (var i = 0, length = elements.length; i < length; i++) {
         element = elements[i];
-        element.appendChild(document.createTextNode(chrome.i18n.getMessage(element.getAttribute('data-i18n'))));
+        element.appendChild(document.createTextNode(chrome.i18n
+                .getMessage(element.getAttribute('data-i18n'))));
     }
 })();
 
@@ -12,11 +13,13 @@ getConfiguration(function(configuration) {
     var elements = document.querySelectorAll('[data-configuration]');
     for (var i = 0, length = elements.length; i < length; i++) {
         element = elements[i];
-        element.value = configuration[element.getAttribute('data-configuration')];
+        element.value = configuration[element
+                .getAttribute('data-configuration')];
 
         element.addEventListener('change', function(event) {
             var target = event.target;
-            configuration[target.getAttribute('data-configuration')] = target.value;
+            configuration[target.getAttribute('data-configuration')] = target
+                    .value;
             chrome.storage.sync.set(configuration);
         });
     }
