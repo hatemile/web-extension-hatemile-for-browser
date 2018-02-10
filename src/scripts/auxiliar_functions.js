52d82b19c7ddf6d27b6d955b3d2f5b53705d77c2
3837162+carlsonsantana@users.noreply.github.com <carlsonsantana@gmail.com>
2018-02-10 01:53:56 -0300

    :recycle: Create a method to load i18n parameters

--- src/scripts/auxiliar_functions.js
+++ src/scripts/auxiliar_functions.js
@@ -140,148 +140,52 @@ function setSimpleAttributes(configuration, userPreferences) {
     }
 }
 
+function setSimpleUserPreferences(keys) {
+    var attribute;
+    var attributes = [
+        'aria-autocomplete-both', 'aria-autocomplete-inline',
+        'aria-autocomplete-list', 'aria-busy-true', 'aria-checked-true',
+        'aria-checked-false', 'aria-checked-mixed', 'aria-dropeffect-copy',
+        'aria-dropeffect-move', 'aria-dropeffect-link',
+        'aria-dropeffect-execute', 'aria-dropeffect-popup',
+        'aria-expanded-true', 'aria-expanded-false', 'aria-grabbed-true',
+        'aria-grabbed-false', 'aria-haspopup-true', 'aria-invalid-true',
+        'aria-level-prefix', 'aria-level-suffix', 'aria-orientation-vertical',
+        'aria-orientation-horizontal', 'aria-pressed-true',
+        'aria-pressed-false', 'aria-pressed-mixed', 'aria-required-true',
+        'aria-selected-true', 'aria-selected-false', 'aria-sort-ascending',
+        'aria-sort-descending', 'aria-sort-other', 'aria-value-maximum-prefix',
+        'aria-value-maximum-suffix', 'aria-value-minimum-prefix',
+        'aria-value-minimum-suffix', 'attribute-title-prefix',
+        'attribute-title-suffix', 'attribute-accesskey-default',
+        'attribute-accesskey', 'attribute-accesskey-prefix',
+        'attribute-accesskey-suffix', 'attribute-target-blank',
+        'attribute-download', 'attribute-data-invalid-url',
+        'attribute-data-invalid-email', 'attribute-data-invalid-range',
+        'attribute-data-invalid-length', 'attribute-data-invalid-pattern',
+        'attribute-data-invalid-required', 'attribute-data-invalid-date',
+        'attribute-data-invalid-time', 'attribute-data-invalid-datetime',
+        'attribute-data-invalid-month', 'attribute-data-invalid-week',
+        'attribute-language-prefix-before', 'attribute-language-suffix-before',
+        'attribute-language-prefix-after', 'attribute-language-suffix-after',
+        'attribute-role-prefix-before', 'attribute-role-suffix-before',
+        'attribute-role-prefix-after', 'attribute-role-suffix-after',
+        'attribute-headers-prefix', 'attribute-headers-suffix',
+        'attribute-longdescription-prefix', 'attribute-longdescription-suffix',
+        'elements-heading', 'skipper-main-content', 'skipper-shortcuts-list',
+        'skipper-table-contents'
+    ];
+
+    for (var i = 0, length = attributes.length; i < length; i++) {
+        attribute = attributes[i];
+
+        keys[attribute] = chrome.i18n.getMessage('extensions_hatemile_'
+            + attribute.replace(/-/g, '_'));
+    }
+}
+
 function getUserPreferences(callback) {
     var keys = {
-        'aria-autocomplete-both': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_aria_autocomplete_both'),
-        'aria-autocomplete-inline': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_aria_autocomplete_inline'),
-        'aria-autocomplete-list': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_aria_autocomplete_list'),
-        'aria-busy-true': chrome.i18n.getMessage('extensions_hatemile_'
-            + 'aria_busy_true'),
-        'aria-checked-true': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_aria_checked_true'),
-        'aria-checked-false': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_aria_checked_false'),
-        'aria-checked-mixed': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_aria_checked_mixed'),
-        'aria-dropeffect-copy': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_aria_dropeffect_copy'),
-        'aria-dropeffect-move': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_aria_dropeffect_move'),
-        'aria-dropeffect-link': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_aria_dropeffect_link'),
-        'aria-dropeffect-execute': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_aria_dropeffect_execute'),
-        'aria-dropeffect-popup': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_aria_dropeffect_popup'),
-        'aria-expanded-true': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_aria_expanded_true'),
-        'aria-expanded-false': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_aria_expanded_false'),
-        'aria-grabbed-true': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_aria_grabbed_true'),
-        'aria-grabbed-false': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_aria_grabbed_false'),
-        'aria-haspopup-true': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_aria_haspopup_true'),
-        'aria-invalid-true': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_aria_invalid_true'),
-        'aria-level-prefix': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_aria_level_prefix'),
-        'aria-level-suffix': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_aria_level_suffix'),
-        'aria-orientation-vertical': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_aria_orientation_vertical'),
-        'aria-orientation-horizontal': chrome.i18n.getMessage('extensio'
-            + 'ns_hatemile_aria_orientation_horizontal'),
-        'aria-pressed-true': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_aria_pressed_true'),
-        'aria-pressed-false': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_aria_pressed_false'),
-        'aria-pressed-mixed': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_aria_pressed_mixed'),
-        'aria-required-true': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_aria_required_true'),
-        'aria-selected-true': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_aria_selected_true'),
-        'aria-selected-false': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_aria_selected_false'),
-        'aria-sort-ascending': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_aria_sort_ascending'),
-        'aria-sort-descending': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_aria_sort_descending'),
-        'aria-sort-other': chrome.i18n.getMessage('extensions_hatemile_'
-            + 'aria_sort_other'),
-        'aria-value-maximum-prefix': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_aria_value_maximum_prefix'),
-        'aria-value-maximum-suffix': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_aria_value_maximum_suffix'),
-        'aria-value-minimum-prefix': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_aria_value_minimum_prefix'),
-        'aria-value-minimum-suffix': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_aria_value_minimum_suffix'),
-        'attribute-title-prefix': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_attribute_title_prefix'),
-        'attribute-title-suffix': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_attribute_title_suffix'),
-        'attribute-accesskey-default': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_attribute_accesskey_default'),
-        'attribute-accesskey': chrome.i18n.getMessage('extensions'
-            + '_hatemile_attribute_accesskey'),
-        'attribute-accesskey-prefix': chrome.i18n.getMessage('extensions'
-            + '_hatemile_attribute_accesskey_prefix'),
-        'attribute-accesskey-suffix': chrome.i18n.getMessage('extensions'
-            + '_hatemile_attribute_accesskey_suffix'),
-        'attribute-target-blank': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_attribute_target_blank'),
-        'attribute-download': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_attribute_download'),
-        'attribute-data-invalid-url': chrome.i18n.getMessage('extensions'
-            + '_hatemile_attribute_data_invalid_url'),
-        'attribute-data-invalid-email': chrome.i18n.getMessage('extensio'
-            + 'ns_hatemile_attribute_data_invalid_email'),
-        'attribute-data-invalid-range': chrome.i18n.getMessage('extensio'
-            + 'ns_hatemile_attribute_data_invalid_range'),
-        'attribute-data-invalid-length': chrome.i18n.getMessage('extensi'
-            + 'ons_hatemile_attribute_data_invalid_length'),
-        'attribute-data-invalid-pattern': chrome.i18n.getMessage('extens'
-            + 'ions_hatemile_attribute_data_invalid_pattern'),
-        'attribute-data-invalid-required': chrome.i18n.getMessage('exten'
-            + 'sions_hatemile_attribute_data_invalid_required'),
-        'attribute-data-invalid-date': chrome.i18n.getMessage('extension'
-            + 's_hatemile_attribute_data_invalid_date'),
-        'attribute-data-invalid-time': chrome.i18n.getMessage('extension'
-            + 's_hatemile_attribute_data_invalid_time'),
-        'attribute-data-invalid-datetime': chrome.i18n.getMessage('exten'
-            + 'sions_hatemile_attribute_data_invalid_datetime'),
-        'attribute-data-invalid-month': chrome.i18n.getMessage('extensio'
-            + 'ns_hatemile_attribute_data_invalid_month'),
-        'attribute-data-invalid-week': chrome.i18n.getMessage('extension'
-            + 's_hatemile_attribute_data_invalid_week'),
-        'attribute-language-prefix-before': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_attribute_language_prefix_before'),
-        'attribute-language-suffix-before': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_attribute_language_suffix_before'),
-        'attribute-language-prefix-after': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_attribute_language_prefix_after'),
-        'attribute-language-suffix-after': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_attribute_language_suffix_after'),
-        'attribute-role-prefix-before': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_attribute_role_prefix_before'),
-        'attribute-role-suffix-before': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_attribute_role_suffix_before'),
-        'attribute-role-prefix-after': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_attribute_role_prefix_after'),
-        'attribute-role-suffix-after': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_attribute_role_suffix_after'),
-        'attribute-headers-prefix': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_attribute_headers_prefix'),
-        'attribute-headers-suffix': chrome.i18n.getMessage('extensions_'
-            + 'hatemile_attribute_headers_suffix'),
-        'attribute-longdescription-prefix': chrome.i18n.getMessage('exte'
-            + 'nsions_hatemile_attribute_longdescription_prefix'),
-        'attribute-longdescription-suffix': chrome.i18n.getMessage('exte'
-            + 'nsions_hatemile_attribute_longdescription_suffix'),
-        'elements-heading': chrome.i18n.getMessage('extensions_hatemile_'
-            + 'elements_heading'),
-        'skipper-main-content': chrome.i18n.getMessage('extensions_hatemile_'
-            + 'skipper_main_content'),
-        'skipper-shortcuts-list': chrome.i18n.getMessage('extensions_hatemile_'
-            + 'skipper_shortcuts_list'),
-        'skipper-table-contents': chrome.i18n.getMessage('extensions_hatemile_'
-            + 'skipper_table_contents'),
         'aria-autocomplete-position': DISPLAY_AFTER,
         'aria-busy-position': DISPLAY_BEFORE,
         'aria-checked-position': DISPLAY_AFTER,
@@ -307,6 +211,9 @@ function getUserPreferences(callback) {
         'attribute-longdescription-position': DISPLAY_AFTER,
         'elements-heading-position': DISPLAY_AFTER
     };
+
+    setSimpleUserPreferences(keys);
+
     if ((typeof browser !== typeof undefined) && (browser.storage) &&
             (browser.storage.local) && (browser.storage.local.get)) {
         var settings = browser.storage.local.get(keys);
